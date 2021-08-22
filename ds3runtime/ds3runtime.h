b36e5ed8792810d10b93c0caeed9311df93cf274
Amir <amirbohdjelian@gmail.com>
2021-08-21 18:14:57 -0700

    ds3runtime.h updates
    Switched to prefer passing a reference instead of a value.

--- ds3runtime/ds3runtime.h
+++ ds3runtime/ds3runtime.h
@@ -12,29 +12,29 @@ class DS3RuntimeScripting
 public:
 	DS3RuntimeScripting();
 
-	void setAsyncMode(bool async);
+	void setAsyncMode(const bool& async);
 
 	void attach();
 
 	bool detach();
 
-	void addHook(std::unique_ptr<Hook> hook);
+	void addHook(std::unique_ptr<Hook>& hook);
 
-	void runScript(std::unique_ptr<ScriptModule> script);
+	void runScript(std::unique_ptr<ScriptModule>& script);
 
-	bool removeScript(uint64_t uniqueId);
+	bool removeScript(const uint64_t& uniqueId);
 
-	bool removeScript(std::string name);
+	bool removeScript(const std::string& name);
 
 	void executeScripts();
 
-	ScriptModule* accessScript(uint64_t scriptUniqueId);
+	ScriptModule* accessScript(const uint64_t& scriptUniqueId);
 
-	ScriptModule* accessScript(std::string name);
+	ScriptModule* accessScript(const std::string& name);
 
-	Hook* accessHook(std::string name);
+	Hook* accessHook(const std::string& name);
 
-	void setGameThreadId(DWORD gameThreadId);
+	void setGameThreadId(const DWORD& gameThreadId);
 
 	DWORD getGameThreadId();
 
