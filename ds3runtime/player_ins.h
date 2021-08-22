4e96dbcc218dfa3888322ecef98fa1457436349a
Amir <amirbohdjelian@gmail.com>
2021-08-22 11:39:41 -0700

    Added even more documentation to PlayerIns

--- ds3runtime/player_ins.h
+++ ds3runtime/player_ins.h
@@ -46,37 +46,95 @@ public:
 
 	void setLeftHandWeapon(const uint32_t& slotNumber, const int32_t& equipParamWeaponId);
 
-	int32_t getRightHandWeapon(const uint32_t& slotNumber);
+	int32_t getRightHandWeapon(const uint32_t& slotNumber) const;
 
 	void setRightHandWeapon(const uint32_t& slotNumber, const int32_t& equipParamWeaponId);
 
-	int32_t getHead();
+	/**
+	* Gets the item in the head slot of this PlayerIns in the game's memory. 
+	* 
+	* @return equipParamId of the protector(Armor) in the head slot.
+	*/
+	int32_t getHead() const;
 
+	/**
+	* Sets the item in the head slot of this PlayerIns in the game's memory. 
+	* 
+	* Setting this value may not result in expected behaivor, to learn more read this header's documentation 
+ 	  at the top of the file.
+	* 
+	* @param equipParamProtectorId equipParamId of the protector(Armor) to put into the head slot.
+	*/
 	void setHead(const int32_t& equipParamProtectorId);
 
-	int32_t getChest();
+	/**
+	* Gets the item in the chest slot of this PlayerIns in the game's memory. 
+	* 
+	* @return equipParamId of the protector(Armor) in the chest slot.
+	*/
+	int32_t getChest() const;
 
-	void setChest(const int32_t&equipParamProtectorId);
+	/**
+	* Sets the item in the chest slot of this PlayerIns in the game's memory. 
+	* 
+	* Setting this value may not result in expected behaivor, to learn more read this header's documentation 
+ 	  at the top of the file.
+	* 
+	* @param equipParamProtectorId equipParamId of the protector(Armor) to put into the chest slot.
+	*/
+	void setChest(const int32_t& equipParamProtectorId);
 
-	int32_t getHands();
+	/**
+	* Gets the item in the hands slot of this PlayerIns in the game's memory. 
+	* 
+	* @return equipParamId of the protector(Armor) in the hands slot.
+	*/
+	int32_t getHands() const;
 
+	/**
+	* Sets the item in the hands slot of this PlayerIns in the game's memory. 
+	* 
+	* Setting this value may not result in expected behaivor, to learn more read this header's documentation 
+ 	  at the top of the file.
+	* 
+	* @param equipParamProtectorId equipParamId of the protector(Armor) to put into the hands slot.
+	*/
 	void setHands(const int32_t& equipParamProtectorId);
 
-	int32_t getLegs();
+	/**
+	* Gets the item in the legs slot of this PlayerIns in the game's memory. 
+	* 
+	* @return equipParamId of the protector(Armor) in the legs slot.
+	*/
+	int32_t getLegs() const;
 
-	/*
+	/**
 	* Sets the item in the legs slot of this PlayerIns in the game's memory. 
 	* 
 	* Setting this value may not result in expected behaivor, to learn more read this header's documentation 
-	* at the top of the file.
+ 	  at the top of the file.
 	* 
-	* @param offsetNumber The PlayerIns number to get the address of
-	* @return The base address of the specified PlayerIns if they exist in memory, otherwise this returns 0.
+	* @param equipParamProtectorId equipParamId of the protector(Armor) to put into the legs slot.
 	*/
 	void setLegs(const int32_t& equipParamProtectorId);
 
-	int32_t getRing(const uint32_t& slotNumber);
+	/**
+	* Gets the item in the specified ring slot of this PlayerIns in the game's memory. 
+	* 
+	* @param slotNumber The ring slot to get the ring from, must be a integer ranging 1-4.
+	* @return equipParamId of the ring in this slot.
+	*/
+	int32_t getRing(const uint32_t& slotNumber) const;
 
+	/**
+	* Sets the item in the specified ring slot of this PlayerIns in the game's memory.
+	*
+	* Setting this value may not result in expected behaivor, to learn more read this header's documentation 
+ 	  at the top of the file.
+	* 
+	* @param slotNumber The ring slot to put the ring in, must be a integer ranging 1-4.
+	* @param equipParamAccessoryId equipParamId of the ring to put in this slot.
+	*/
 	void setRing(const uint32_t& slotNumber, const int32_t& equipParamAccessoryId);
 
 	/**
@@ -85,7 +143,7 @@ public:
 	* 
 	* @return True if the "No Goods Consume" debug flag is set for this Player.
 	*/
-	bool isNoGoodsConsume();
+	bool isNoGoodsConsume() const;
 	
 	/**
 	* Sets the "No Goods Consume" debug flag on or off for this Player. This flag prevents consumption of inventory goods which may allow them to be used 
@@ -95,6 +153,11 @@ public:
 	*/
 	void setNoGoodsConsume(const bool& value);
 
+	/**
+	* Gets the virtual address of this Player's PlayerCtrl object.
+	* 
+	* @return Virtual address of this Player's PlayerCtrl object.
+	*/
 	uintptr_t getPlayerCtrl();
 
 	/**
@@ -104,7 +167,14 @@ public:
 	*/
 	uintptr_t* getNetworkHandle();
 
-	bool isValid();
+	/**
+	* Check if this refers to a valid PlayerIns object.
+	* 
+	* Peformes several multi-level pointer checks.
+	* 
+	* @return True if PlayerIns is valid.
+	*/
+	bool isValid() const;
 
 	/**
 	* Get the base address of PlayerIns by the offset multiplier in memory. 
