f4f704546b7b1180fe4181553f42bc11b5f347fc
Amir <amirbohdjelian@gmail.com>
2021-08-22 14:52:13 -0700

    Added const to getters

--- ds3runtime/game_data_man.h
+++ ds3runtime/game_data_man.h
@@ -25,7 +25,7 @@ public:
 	* 
 	* @return The number of times the game has been finished on this save game, also known as "New Game Level".
 	*/
-	int32_t getClearCount();
+	int32_t getClearCount() const;
 
 	/**
 	* Sets the number of times the game has been finished on this save game, also known as "New Game Level".
@@ -39,28 +39,28 @@ public:
 	* 
 	* @return The number of successful co-op sessions the player has had a summoned phantom(Eg. Critical battle won) on this save game.
 	*/
-	int32_t getRescueWhite();
+	int32_t getRescueWhite() const;
 
 	/**
 	* Gets the number of Dark Spirits that have been slain on this save game.
 	* 
 	* @return The number of Dark Spirits the player has slain on this save game.
 	*/
-	int32_t getKillBlack();
+	int32_t getKillBlack() const;
 
 	/**
 	* Gets the number of times the player has gotten a "You Died" on this save game.
 	* 
 	* @return The number of times the player has gotten a "You Died" on this save game.
 	*/
-	int32_t getDeathNum();
+	int32_t getDeathNum() const;
 
 	/**
 	* Gets the number of milliseconds spent playing on this save game.
 	* 
 	* @return The number of milliseconds spent playing on this save game.
 	*/
-	uint32_t getPlayTime();
+	uint32_t getPlayTime() const;
 
 	/**
 	* Gets the one instance of GameDataMan if one exists.
