558074d1a05ccc8f3d786c0de6625228f1471ff7
Amir <amirbohdjelian@gmail.com>
2021-08-22 12:34:58 -0700

    Added documentation to Hook

--- ds3runtime/hook.h
+++ ds3runtime/hook.h
@@ -8,13 +8,33 @@ public:
 	Hook(uintptr_t original, uintptr_t replacement);
 
 	~Hook();
-
+	
+	/**
+	 * Preforms the attaching of this hook via a Detours transaction.
+	 *
+	 * @return The result of the Detours transaction, true if no error.
+	 */
 	bool install();
 
+	/**
+	 * Preforms the detaching of this hook via a Detours transaction.
+	 *
+	 * @return The result of the Detours transaction, true if no error.
+	 */
 	bool uninstall();
 
+	/**
+	 * Gets the name of this hook.
+	 *
+	 * @return The name of this hook.
+	 */
 	virtual std::string getName() = 0;
 
+	/**
+	 * Gets a pointer to the original function that this hook replaces.
+	 *
+	 * @return Pointer to the original function.
+	 */
 	uintptr_t* getOriginal();
 protected:
 	uintptr_t* original;
